3a5e252dd4edd9d47368bfe559d8a3efa815b407
yazed_hassan <108896411+Yazed20@users.noreply.github.com>
2024-07-16 12:38:45 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,26 +5,22 @@ versionCode=$2
 
 # Check for decimal in arguments
 for arg in "$@"; do
-	if [[ $arg =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
-		true
-	else
-		echo "> Arguments must be number"
-		exit 1
-	fi
+    if [[ $arg =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+        true
+    else
+        echo "> Arguments must be number"
+        exit 1
+    fi
 done
 
 # Extract version information from module.prop
-last_version=$(grep -o 'version=v[0-9.]*' module.prop |
-	cut -d'=' -f2 | sed 's/v//')
-
 if [ -z "$version" ]; then
-	version=$(grep -o 'version=v[0-9.]*' module.prop |
-		cut -d'=' -f2 | sed 's/v//')
+    version=$(grep -o 'version=v[0-9.]*' module.prop | cut -d'=' -f2 | sed 's/v//')
 fi
 
 if [ -z "$versionCode" ]; then
-	versionCode=$(grep versionCode module.prop | cut -d '=' -f2)
-	versionCode=$((versionCode + 1))
+    versionCode=$(grep versionCode module.prop | cut -d '=' -f2)
+    versionCode=$((versionCode + 1))
 fi
 
 # Update module.prop with the new version and versionCode
@@ -35,9 +31,9 @@ module_name=$(sed -n 's/^id=\(.*\)/\1/p' module.prop)
 
 # Create a zip package
 7za a "packages/$module_name-v${version}_$versionCode-beta.zip" \
-	META-INF \
-	fmiop* \
-	customize.sh \
-	module.prop \
-	service.sh \
-	cleaner.zip
+    META-INF \
+    fmiop* \
+    customize.sh \
+    module.prop \
+    service.sh \
+    cleaner.zip
