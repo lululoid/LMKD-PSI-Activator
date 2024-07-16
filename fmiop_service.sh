785331224f6801d31ca093f6a0da180b04b205df
yazed_hassan <108896411+Yazed20@users.noreply.github.com>
2024-07-16 12:37:53 +0300

    Update fmiop_service.sh

--- fmiop_service.sh
+++ fmiop_service.sh
@@ -1,12 +1,41 @@
 #!/system/bin/sh
-# shellcheck disable=SC3043,SC2034,SC2086,SC3060,SC3010,SC2046
 
-[[ "$LOG_ENABLED" = "true" ]] && {
-	exec 3>&1 1>>"$NVBASE/fmiop.log" 2>&1
-	set -x # Prints commands, prefixing them with a character stored in an environmental variable ($PS4)
+log="$MODPATH/log.txt"
+BIN=/system/bin
+totalmem=$(free | awk '/^Mem:/ {print $2}')
+low_mem_device=false
+swap_enabled=false
+
+swap_filename=$NVBASE/fmiop_swap
+swapfile() {
+    swapoff -a
+    dd if=/dev/zero of="$swap_filename" bs=1024 count=$swap_size
+    mkswap -L meZram-swap "$swap_filename"
+    swapon -L meZram-swap
+    swap_enabled=true
 }
 
-. $MODPATH/fmiop.sh
+ram_swap() {
+    if [ "$totalmem" -lt 2097152 ]; then
+        swap_size=$(awk -v size="$totalmem" 'BEGIN { printf "%.0f\n", size * 0.55 }')
+        swapfile
+    fi
+}
 
-kill $(resetprop fmiop.pid)
-fmiop
+apply_tweaks() {
+    if [ "$totalmem" -lt 2097152 ]; then
+        log "Low memory device, applying tweaks"
+        low_mem_device=true
+    else
+        log "High memory device, no tweaks needed"
+    fi
+}
+
+main() {
+    apply_tweaks
+    if $low_mem_device && ! $swap_enabled; then
+        ram_swap
+    fi
+}
+
+main
