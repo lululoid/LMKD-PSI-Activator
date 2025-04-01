8762556697c9618b423208174f6e1e7793faf6e4
lululoid <ereiwn@gmail.com>
2025-04-01 18:33:26 +0700

    Fix?: Attempt to fix uninstall.sh bug on some device

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,15 @@
 #!/system/bin/sh
 # shellcheck disable=SC3010,SC3060,SC3043,SC2086,SC2046
 script_name=$(basename "$0")
+timeout=180 #timeout for 3 minutes
 
 until [ "$(resetprop sys.boot_completed)" -eq 1 ] && [ -d /sdcard/Android/fmiop ]; do
-	sleep 5
+	timeout=$((timeout - 1))
+	if [ $timeout -eq 0 ]; then
+		break
+	fi
+
+	sleep 1
 done
 
 exec 3>&1 1>>"/sdcard/fmiop_${script_name%.sh}.log" 2>&1
